7de75d0f14c8db76f46ded88376433eb08027d10
mansiksohn <mansiksohn@gmail.com>
2024-10-26 00:27:27 +0900

    버튼에 type="button" 추가 시도해보기

--- src/components/ShareSection.js
+++ src/components/ShareSection.js
@@ -4,7 +4,7 @@ import '../styles/share.scss';
 function ShareSection({ handleCopyLink, buttonText, buttonColor }) {
   return (
     <div className="share-section mt-6">
-      <button onClick={handleCopyLink} className={`${buttonColor} text-white p-2 rounded w-full`}>
+      <button type="button" onClick={handleCopyLink} className={`${buttonColor} text-white p-2 rounded w-full`}>
         {buttonText}
       </button>
     </div>
