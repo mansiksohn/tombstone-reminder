08f0e2b775d3d9a14dfc3945a41a7dd847d52415
mansiksohn <mansiksohn@gmail.com>
2024-10-26 21:04:09 +0900

    복사기능 대신 a태그를 사용해 링크로 이동하는 기능으로 변경

--- src/components/ShareSection.js
+++ src/components/ShareSection.js
@@ -1,23 +1,9 @@
-import React, { useState } from 'react';
-import '../styles/share.scss';
-
-function ShareSection({ handleCopyLink, buttonText, buttonColor }) {
-  // `isPressed` 상태 생성
-  const [isPressed, setIsPressed] = useState(false);
-
+function ShareSection({ link, buttonText, buttonColor }) {
   return (
     <div className="share-section mt-6">
-      <button
-        type="button"
-        onClick={handleCopyLink}
-        onTouchStart={() => setIsPressed(true)} // 터치 시작 시 `isPressed` 설정
-        onTouchEnd={() => setIsPressed(false)} // 터치 종료 시 `isPressed` 해제
-        onMouseDown={() => setIsPressed(true)} // 마우스로 눌렀을 때도 적용
-        onMouseUp={() => setIsPressed(false)} // 마우스에서 손을 뗄 때 해제
-        className={`${buttonColor} text-white p-2 rounded w-full ${isPressed ? 'pressed' : ''}`}
-      >
+      <a href={link} className={`${buttonColor} text-white p-2 rounded w-full text-center block`}>
         {buttonText}
-      </button>
+      </a>
     </div>
   );
 }
