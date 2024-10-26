5f5a9c7b99e42d6d4d44732a468c5af4488cec40
mansiksohn <mansiksohn@gmail.com>
2024-10-26 20:50:26 +0900

    공유버튼 동작 테스트용

--- src/components/ShareSection.js
+++ src/components/ShareSection.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/share.scss';
 
 function ShareSection({ handleCopyLink, buttonText, buttonColor }) {
+  // `isPressed` 상태 생성
+  const [isPressed, setIsPressed] = useState(false);
+
   return (
     <div className="share-section mt-6">
-      <button type="button" onClick={handleCopyLink} className={`${buttonColor} text-white p-2 rounded w-full`}>
+      <button
+        type="button"
+        onClick={handleCopyLink}
+        onTouchStart={() => setIsPressed(true)} // 터치 시작 시 `isPressed` 설정
+        onTouchEnd={() => setIsPressed(false)} // 터치 종료 시 `isPressed` 해제
+        onMouseDown={() => setIsPressed(true)} // 마우스로 눌렀을 때도 적용
+        onMouseUp={() => setIsPressed(false)} // 마우스에서 손을 뗄 때 해제
+        className={`${buttonColor} text-white p-2 rounded w-full ${isPressed ? 'pressed' : ''}`}
+      >
         {buttonText}
       </button>
     </div>
