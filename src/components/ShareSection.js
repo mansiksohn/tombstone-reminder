fab180b0adb65e482a2d5f7477377a4a8cb065ba
mansiksohn <mansiksohn@gmail.com>
2024-10-27 17:48:35 +0900

    ShareSection 충돌 해결

--- src/components/ShareSection.js
+++ src/components/ShareSection.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
 function ShareSection({ link, linkText = "공유할 페이지 열기" }) {
   return (
     <div className="share-section mt-6">
@@ -11,13 +10,6 @@ function ShareSection({ link, linkText = "공유할 페이지 열기" }) {
         className="text-blue-500 underline" // 기본 하이퍼링크 스타일
       >
         {linkText}
-=======
-function ShareSection({ link, buttonText, buttonColor }) {
-  return (
-    <div className="share-section mt-6">
-      <a href={link} className={`${buttonColor} text-white p-2 rounded w-full text-center block`}>
-        {buttonText}
->>>>>>> testView
       </a>
     </div>
   );
