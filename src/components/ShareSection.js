d1ad107b2d91e4511ca5f0e0ec8ed58d63e69f56
mansiksohn <mansiksohn@gmail.com>
2024-10-27 12:30:21 +0900

    link 가 맞음

--- src/components/ShareSection.js
+++ src/components/ShareSection.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../styles/share.scss';
 
-function ShareSection({ handleCopyLink, buttonText, buttonColor }) {
+function ShareSection({ link, buttonText, buttonColor }) {
   return (
     <div className="share-section mt-6">
-      <a href={handleCopyLink} className={`${buttonColor} text-white p-2 rounded w-full text-center block`}>
+      <a href={link} className={`${buttonColor} text-white p-2 rounded w-full text-center block`}>
         {buttonText}
       </a>
     </div>
