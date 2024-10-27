4f0db2bbe59c03e05422e6fa99a981e4b607dffc
SMS <mansiksohn@gmail.com>
2024-10-27 14:39:52 +0900

    디깅용 콘솔로그 코드 삭제

--- src/components/ShareSection.js
+++ src/components/ShareSection.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../styles/share.scss';
 
 function ShareSection({ link, buttonText, buttonColor }) {
-  console.log('Received link in ShareSection:', link); // ShareSection에서 link 확인
   return (
     <div className="share-section mt-6">
       <a href={link} className={`${buttonColor} text-white p-2 rounded w-full text-center block`}>
